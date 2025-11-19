408cf036aae04eb9f0a8a7d1e411230f69423920
cdcsccc <468027952@qq.com>
2025-11-18 19:39:30 -0500

    Task 1.1 :Fix Chenckstyle issue

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,29 @@
 package theater;
 
+/**
+ * Class representing a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private final String name;
+    private final String type;
 
+    /**
+     * Creates a Play.
+     *
+     * @param name name of the play
+     * @param type type of the play (e.g., tragedy, comedy)
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
     }
 }
